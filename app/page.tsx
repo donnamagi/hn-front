b79c9df91464c14a0b4912f4df4f4cdb68740191
Donna Mägi <magi.donna@gmail.com>
2024-06-04 18:56:15 +0200

    improve imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link'
 import React from 'react'
-import Feed from '../components/Feed'
+import Feed from '@/components/Feed'
 
 export default function Page() {
   return (
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import Article from './Article'
+import Article from '@/components/Article'
 
 interface Article {
   by: string
