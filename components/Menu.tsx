0466dfc548ff4f54675da4c56d9ae77721fd5ab7
Donna Mägi <magi.donna@gmail.com>
2024-06-06 18:44:00 +0300

    ref, btn component

---
+++ components/Menu.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
+
+const LINKS = [
+  { href: '/', label: 'Best' },
+  { href: '/top', label: 'Top' },
+  { href: '/new', label: 'New' },
+  { href: '/ask', label: 'Ask HN' },
+  { href: '/show', label: 'Show HN' },
+  { href: '/jobs', label: 'Jobs' }
+]
+
+export const MainMenu = () => (
+  <div className='flex w-full flex-col text-sm'>
+    <div className='flex flex-col gap-4'>
+      <Link
+        href='/'
+        className={
+          (cn('link-card inline-flex items-center gap-2 p-2'),
+          buttonVariants({ variant: 'ghost' }))
+        }
+      >
+        <div className='flex flex-col text-center'>
+          <span className='font-semibold text-center'>HN</span>
+        </div>
+      </Link>
+      <div className='flex flex-col gap-1'>
+        {LINKS.map((link, linkIndex) => (
+          <Link
+            key={linkIndex}
+            href={link.href}
+            className={buttonVariants({ variant: 'ghost' })}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  </div>
+)
--- components/Sidemenu.tsx
+++ components/Sidemenu.tsx
@@ -1,36 +1,26 @@
 'use client'
 
-import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { ScrollArea } from '@/components/Scrollarea'
 
 interface SideMenuProps {
   title?: string
   isInner?: boolean
-  links?: { href: string; label: string }[]
   className?: string
+  children: React.ReactNode
 }
 
-const LINKS = [
-  { href: '/', label: 'Best' },
-  { href: '/top', label: 'Top' },
-  { href: '/new', label: 'New' },
-  { href: '/ask', label: 'Ask HN' },
-  { href: '/show', label: 'Show HN' },
-  { href: '/jobs', label: 'Jobs' }
-]
-
-export const SideMenu = ({
+export default function Sidemenu({
   title,
   isInner,
-  links,
-  className
-}: SideMenuProps) => {
+  className,
+  children
+}: SideMenuProps) {
   return (
     <ScrollArea
       className={cn(
         'hidden bg-slate-50 lg:flex lg:flex-col lg:border-r',
-        isInner ? 'lg:w-80 xl:w-96' : 'lg:w-60 xl:w-72',
+        isInner ? 'lg:w-80 xl:w-96' : 'lg:w-40 xl:w-60',
         className
       )}
     >
@@ -40,31 +30,10 @@ export const SideMenu = ({
             <span className='text-sm font-semibold tracking-tight'>
               {title}
             </span>
-            <div className='flex items-center gap-2'></div>
           </div>
         </div>
       )}
-      <div className='bg-zinc-50 p-3 h-dvh'>
-        <div className='flex w-full flex-col text-sm'>
-          <div className='flex flex-col gap-4'>
-            <Link
-              href='/'
-              className='link-card inline-flex items-center gap-2 p-2'
-            >
-              <div className='flex flex-col'>
-                <span className='font-semibold tracking-tight'>HN</span>
-              </div>
-            </Link>
-            <div className='flex flex-col gap-1'>
-              {LINKS.map((link, linkIndex) => (
-                <Link key={link.href} href={link.href}>
-                  {link.label}
-                </Link>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
+      <div className='bg-zinc-50 p-3 h-dvh'>{children}</div>
     </ScrollArea>
   )
 }
