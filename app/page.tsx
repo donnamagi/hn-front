82d1a4a149a520965ecdbd8d352f006f6ad98d0c
Donna Mägi <magi.donna@gmail.com>
2024-06-08 21:58:06 +0300

    preloading stories on index load + wip page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+'use client'
 import React from 'react'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import Link from 'next/link'
+import { buttonVariants } from '@/components/ui/button'
+import { preloadStories } from '@/lib/utils'
 
 export default function Page() {
+  preloadStories()
   return (
     <ScrollArea>
       <div className='content-wrapper'>
-        <div className='content'></div>
+        <div className='content'>
+          <div className='flex flex-col items-center justify-center min-h-screen'>
+            <h1 className='mb-4'>Nothing here yet :)</h1>
+            <Link
+              href='/top'
+              className={buttonVariants({ variant: 'outline' })}
+            >
+              See top stories
+            </Link>
+          </div>
+        </div>
       </div>
     </ScrollArea>
   )
--- lib/utils.ts
+++ lib/utils.ts
@@ -6,9 +6,6 @@ import { CommentType } from '@/components/Comment';
 
 const cacheStore: { [key: string]: any } = {};
 
-export const getFromCache = (key: string) => {
-  return cacheStore[key];
-};
 
 export const setInCache = (key: string, value: any) => {
   cacheStore[key] = value;
@@ -18,7 +15,12 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const fetchStoryIds = cache(async (category:string): Promise<number[]> => {
+export const fetchStoryIds = async (category:string): Promise<number[]> => {
+  const cacheKey = `${category}`;
+  if (cacheStore && cacheStore[cacheKey]) {
+    return cacheStore[cacheKey];
+  }
+
   try {
     const response = await fetch(
       `https://hacker-news.firebaseio.com/v0/${category}stories.json`
@@ -27,21 +29,21 @@ export const fetchStoryIds = cache(async (category:string): Promise<number[]> =>
       throw new Error('Network response was not ok');
     }
     const data = await response.json();
-    return data.slice(0, 30);
+    const sliced = data.slice(0, 30);
+    setInCache(cacheKey, sliced);
+    return sliced;
   } catch (error) {
     console.error('Error fetching story IDs:', error);
     return [];
   }
-});
+};
 
-export const fetchArticle = cache(async (storyId: number): Promise<ArticleType> => {
+export const fetchArticle = async (storyId: number): Promise<ArticleType> => {
   const cacheKey = `${storyId}`;
-  const cache = getFromCache(cacheKey);
-
-  if (cache) {
-    console.log(`Returning cached article: ${storyId}`);
-    return cache;
+  if (cacheStore && cacheStore[cacheKey]) {
+    return cacheStore[cacheKey];
   }
+
   try {
     const response = await fetch(
       `https://hacker-news.firebaseio.com/v0/item/${storyId}.json`
@@ -49,22 +51,19 @@ export const fetchArticle = cache(async (storyId: number): Promise<ArticleType> 
     if (!response.ok) {
       throw new Error('Network response was not ok')
     }
-    const data: ArticleType = await response.json()
+    const data = await response.json()
     setInCache(cacheKey, data);
     return data
   } catch (error) {
     console.error('Error fetching article:', error)
     return {} as ArticleType
   }
-})
+}
 
 export const fetchComments = cache(async (storyId: number, kids: number[]): Promise<CommentType[]> => {
   const cacheKey = `comments_${storyId}`;
-  const cache = getFromCache(cacheKey);
-
-  if (cache) {
-    console.log(`Returning cached comments for: ${storyId}`);
-    return cache;
+  if (cacheStore && cacheStore[cacheKey]) {
+    return cacheStore[cacheKey];
   }
 
   try {
@@ -88,3 +87,16 @@ export const fetchComments = cache(async (storyId: number, kids: number[]): Prom
     return [];
   }
 });
+
+export const preloadStories = () => {
+  try {
+    void fetchStoryIds('top').then((data) => {
+      data.forEach((storyId) => { 
+        void fetchArticle(storyId) 
+      })
+    })
+  } catch (error) {
+    console.info(error)
+    return []
+  }
+}
