818f6c243499a7f794e6554a4d1b38a18015510d
Donna Mägi <magi.donna@gmail.com>
2024-06-09 17:06:10 +0300

    smoother transitions

--- components/Article.tsx
+++ components/Article.tsx
@@ -6,6 +6,7 @@ import { DecodedTextArea } from '@/components/Comment'
 import Link from 'next/link'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Comment, CommentType } from '@/components/Comment'
+import { cn } from '@/lib/utils'
 
 export interface ArticleType {
   by: string
@@ -35,6 +36,7 @@ interface ArticleProps {
 export function Article({ storyId }: ArticleProps) {
   const [article, setArticle] = useState<ArticleType | null>(null)
   const [comments, setComments] = useState<CommentType[] | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const getArticle = async () => {
@@ -53,52 +55,61 @@ export function Article({ storyId }: ArticleProps) {
         setComments(comments)
       } catch (err) {
         console.error('Error fetching comments:', err)
+      } finally {
+        setLoading(false)
       }
     }
 
     getArticle()
   }, [storyId])
 
-  if (!article) {
-    return <div>Loading article...</div>
-  }
-
   const domain = article?.url ? new URL(article.url).hostname : null
 
   return (
     <ScrollArea>
       <div className='py-10'>
-        <h4 className='text-xl md:text-3xl font-bold text-neutral-800'>
-          {article.title}
-        </h4>
-        <p className=''>
-          {article.descendants} points by {article.by}
-        </p>
-        {article.url && (
-          <Link
-            href={article.url}
-            rel='noopener noreferrer'
-            className='underline text-sm'
-          >
-            {domain}
-          </Link>
-        )}
-      </div>
-      <hr />
-      {article.text && (
-        <div>
-          <p className='py-10'>
-            <DecodedTextArea text={article.text} />
-          </p>
-          <hr />
+        <div
+          className={`transition-all duration-100 ${
+            loading ? 'opacity-0' : 'opacity-100'
+          }`}
+        >
+          {article && (
+            <div>
+              <h4 className='text-xl md:text-3xl font-bold text-neutral-800'>
+                {article.title}
+              </h4>
+              <p>
+                {article.descendants} points by {article.by}
+              </p>
+              {article.url && (
+                <Link
+                  href={article.url}
+                  rel='noopener noreferrer'
+                  className='underline text-sm'
+                >
+                  {domain}
+                </Link>
+              )}
+              <hr />
+              {article.text && (
+                <div>
+                  <p className='py-10'>
+                    <DecodedTextArea text={article.text} />
+                  </p>
+                  <hr />
+                </div>
+              )}
+              <div className='py-4'>
+                {comments &&
+                  comments.map((comment) => (
+                    <div key={comment.id} className='py-5'>
+                      <Comment comment={comment} />
+                    </div>
+                  ))}
+              </div>
+            </div>
+          )}
         </div>
-      )}
-      <div className='py-4'>
-        {comments?.map((comment) => (
-          <div key={comment.id} className='py-5'>
-            <Comment comment={comment} />
-          </div>
-        ))}
       </div>
     </ScrollArea>
   )
--- components/FeedItem.tsx
+++ components/FeedItem.tsx
@@ -10,6 +10,7 @@ interface FeedItemProps {
 
 export function FeedItem({ storyId, category }: FeedItemProps) {
   const [article, setArticle] = useState<ArticleType | null>(null)
+  const [loading, setLoading] = useState(true)
 
   const getArticle = async () => {
     try {
@@ -17,27 +18,33 @@ export function FeedItem({ storyId, category }: FeedItemProps) {
       setArticle(article)
     } catch (err) {
       console.error('Error fetching article:', err)
+    } finally {
+      setLoading(false)
     }
   }
 
   getArticle()
 
-  if (!article) {
-    return <div>Loading article...</div>
-  }
-
   const domain = article?.url ? new URL(article.url).hostname : null
 
   return (
-    <Navlink href={`/${category}/${storyId}`}>
-      <h4 className='text-base font-bold'>{article.title}</h4>
-      <span>
-        {article.url && <span>{domain} | </span>}
-        by {article.by}
-        {article.descendants !== 0
-          ? ` | ${article.descendants} comments`
-          : null}
-      </span>
-    </Navlink>
+    <div
+      className={`transition-all duration-100 ${
+        loading ? 'opacity-0' : 'opacity-100'
+      }`}
+    >
+      {article && (
+        <Navlink href={`/${category}/${storyId}`}>
+          <h4 className='text-base font-bold'>{article.title}</h4>
+          <span>
+            {article.url && <span>{domain} | </span>}
+            by {article.by}
+            {article.descendants !== 0
+              ? ` | ${article.descendants} comments`
+              : null}
+          </span>
+        </Navlink>
+      )}
+    </div>
   )
 }
