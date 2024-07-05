3e30f4e17dc7e069f9ec45661d95ebfb2779ffac
Donna Mägi <magi.donna@gmail.com>
2024-07-05 15:03:41 +0200

    toggling keywords

--- components/Keywords.tsx
+++ components/Keywords.tsx
@@ -2,9 +2,11 @@
 
 import React, { useEffect, useState } from 'react'
 import { fetchTopKeywords } from '@/lib/utils'
+import { Toggle } from '@/components/ui/toggle'
 
 export function Keywords() {
   const [keywords, setKeywords] = useState([])
+  const [interests, setInterests] = useState<string[]>([])
 
   useEffect(() => {
     const fetchKeywords = async () => {
@@ -20,20 +22,34 @@ export function Keywords() {
   }, [])
 
   return (
-    <>
-      <h1 className='text-lg md:text-xl font-bold my-5'>
-        Most common keywords on HN
+    <div className='my-5'>
+      <h1 className='text-lg md:text-xl font-bold'>
+        Most common topics this week
       </h1>
+      <p>Check your interests to get a personalized feed of articles.</p>
       {keywords.length !== 0 && (
-        <div className='gap-2 flex flex-wrap'>
-          {keywords.map((keyword: any) => (
-            <span key={keyword[0]} className='badge'>
-              {keyword[0]}{' '}
-            </span>
+        <div className='gap-2 flex flex-wrap mt-3'>
+          {keywords.map((keyword) => (
+            <Toggle
+              key={keyword[0]}
+              className='badge'
+              size={'sm'}
+              onClick={() => {
+                if (interests.includes(keyword[0])) {
+                  setInterests(
+                    interests.filter((i: string) => i !== keyword[0])
+                  )
+                } else {
+                  setInterests([...interests, keyword[0]])
+                }
+              }}
+            >
+              {keyword[0]}
+            </Toggle>
           ))}
         </div>
       )}
-    </>
+    </div>
   )
 }
 
