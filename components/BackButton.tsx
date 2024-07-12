15079f5d1b549a1c83df992ac884354f3585321d
Donna Mägi <magi.donna@gmail.com>
2024-07-12 14:54:25 +0200

    showing back button if referred from same domain

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,18 +1,27 @@
 'use client'
 
+import { useLayoutEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import { ArrowLeftIcon } from '@radix-ui/react-icons'
 
 export default function BackButton() {
+  const [showButton, setShowButton] = useState(false)
   const router = useRouter()
 
-  const handleGoBack = () => {
-    router.back()
+  useLayoutEffect(() => {
+    const comingFromSameDomain = document.referrer.includes(
+      window.location.origin
+    )
+    setShowButton(comingFromSameDomain)
+  }, [])
+
+  if (!showButton) {
+    return null
   }
 
   return (
-    <Button variant='outline' size={'sm'} onClick={handleGoBack}>
+    <Button variant='outline' size='sm' onClick={() => router.back()}>
       <ArrowLeftIcon />
     </Button>
   )
